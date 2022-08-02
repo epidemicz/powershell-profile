bbd4401c75347bf8b2862a665ae1febd290b4958
Timothy Zenner <epidemicz@gmail.com>
2022-08-01 21:13:56 -0500

    updated psreadline option

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@ oh-my-posh init pwsh --config C:\Users\timze\AppData\Local\Programs\oh-my-posh\t
 [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
 
 # PS-ReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle Visual
